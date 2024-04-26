aecbac09153bfe8d4a60abd1ac9a26540ef24243
JKilgour <161059184+JKilgour@users.noreply.github.com>
2024-04-26 16:33:42 +0100

    Add files via upload

---
+++ SummerProject/JavaFiles/Grid.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Grid{
+
+    public static void main(String[] args){
+        JFrame a = new JFrame();
+
+       
+        a.setSize(500,500);
+        a.setVisible(true);
+    }
+}
