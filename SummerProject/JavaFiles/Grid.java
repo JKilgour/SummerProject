9964245a9ff78f10728a7e60f918a75e0e79306c
JKilgour <j.kilgour@lancaster.ac.uk>
2024-04-29 22:55:13 +0100

    Buttons

--- SummerProject/JavaFiles/Grid.java
+++ SummerProject/JavaFiles/Grid.java
@@ -3,17 +3,22 @@ import java.awt.*;
 
 public class Grid{
    
-        JFrame a = new JFrame();
-        JButton b = new JButton();
-
-       public Grid(){
-        a.setSize(800,800);
-        a.setVisible(true);
-        for(int i=0;i < 14;i++){
-            for(int x=0; x  <14;x++){
-
-            }
-        }
-       }
+    JFrame a = new JFrame();
+    JButton b;
     
+
+   public Grid(){
+    a.setSize(800,800);
+    a.setVisible(true);
+    for(int i=0;i < 14;i++){
+        for(int x=0; x  <14;x++){
+            b = new JButton();
+            b.setBounds(50+x*50,50+i*50,50,50);
+            b.setBackground(Color.white);
+            a.add(b);
+            b.setVisible(true);
+        }
+    }
+   }
+
 }
