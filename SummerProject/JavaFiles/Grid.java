a8fb9d67c55b60ac2822b0a4890b06657bb7bdea
JKilgour <j.kilgour@lancaster.ac.uk>
2024-04-30 15:23:52 +0100

    fixed buttons

--- SummerProject/JavaFiles/Grid.java
+++ SummerProject/JavaFiles/Grid.java
@@ -1,24 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Grid{
    
-    JFrame a = new JFrame();
-    JButton b;
+    JFrame a = new JFrame("Nanogram");
+    private JButton[][] buttons ;
+
     
 
    public Grid(){
     a.setSize(800,800);
     a.setVisible(true);
-    for(int i=0;i < 14;i++){
-        for(int x=0; x  <14;x++){
-            b = new JButton();
-            b.setBounds(50+x*50,50+i*50,50,50);
-            b.setBackground(Color.white);
-            a.add(b);
-            b.setVisible(true);
+    a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JPanel gridPanel = new JPanel(new GridLayout(15, 15, 0, 0));
+    buttons = new JButton[15][15];
+    a.add(gridPanel);
+
+    for(int i=0;i < 15;i++){
+        for(int x=0; x  <15;x++){
+            JButton button = new JButton();
+            buttons[i][x] = button;
+            gridPanel.add(button);
+            button.setVisible(true);
         }
     }
-   }
+    a.setVisible(true);
 
+
+   }
 }
