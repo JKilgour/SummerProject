1281c17a38385c7c27940c4c1546ea5719208668
JKilgour <j.kilgour@lancaster.ac.uk>
2024-05-03 16:07:46 +0100

    Small change

--- SummerProject/JavaFiles/Grid.java
+++ SummerProject/JavaFiles/Grid.java
@@ -10,15 +10,15 @@ public class Grid{
     
 
    public Grid(){
-    a.setSize(800,800);
+    a.setSize(900,900);
     a.setVisible(true);
     a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JPanel gridPanel = new JPanel(new GridLayout(15, 15, 0, 0));
-    buttons = new JButton[15][15];
+    JPanel gridPanel = new JPanel(new GridLayout(16, 16, 0, 0));
+    buttons = new JButton[16][16];
     a.add(gridPanel);
 
-    for(int i=0;i < 15;i++){
-        for(int x=0; x  <15;x++){
+    for(int i=0;i < 16;i++){
+        for(int x=0; x  <16;x++){
             JButton button = new JButton();
             buttons[i][x] = button;
             gridPanel.add(button);
