7018773c67bd943318b85044be9edc7f38210f8e
JKilgour <j.kilgour@lancaster.ac.uk>
2024-05-09 13:32:44 +0100

    Removed gaps between buttons and added space on the top and left side

--- SummerProject/JavaFiles/Grid.java
+++ SummerProject/JavaFiles/Grid.java
@@ -13,12 +13,13 @@ public class Grid{
     a.setSize(900,900);
     a.setVisible(true);
     a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JPanel gridPanel = new JPanel(new GridLayout(16, 16, 0, 0));
+    JPanel gridPanel = new JPanel(new GridLayout(15, 15, -5, -5)); //creates a layout for the grid with the gaps non existent between buttons
+    gridPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 0, 0)); //creates a gap between the top and left
     buttons = new JButton[16][16];
     a.add(gridPanel);
 
-    for(int i=0;i < 16;i++){
-        for(int x=0; x  <16;x++){
+    for(int i=0;i < 15;i++){
+        for(int x=0; x  <15;x++){
             JButton button = new JButton();
             buttons[i][x] = button;
             gridPanel.add(button);
